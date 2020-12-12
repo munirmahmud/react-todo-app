d93bdaa4b0e184a9514fe60a329f0529b08d3025
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-12 12:31:45 +0600

    Input field added

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { FaBook } from 'react-icons/fa';
 
-const TodoInput = () => {
+const TodoInput = ({item, handleChange, handleSubmit, editItem}) => {
     return (
-        <div>
-            Todo Input
+        <div className="card card-body my-3">
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text bg-primary text-white">
+                            <FaBook />
+                        </div>
+                    </div>
+
+                    <input 
+                        type="text" 
+                        className="form-control text-capitalize" 
+                        placeholder="Add to Do"
+                        value={item}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <button className="btn btn-block btn-primary mt-3">Add Item</button>
+            </form>
         </div>
     )
 }
